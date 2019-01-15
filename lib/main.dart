123547b354f7677c49cb38c61f3d30b7b4d0db48
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2019-01-15 03:06:53 -0800

    added favorite buttons next to the names

--- lib/main.dart
+++ lib/main.dart
@@ -15,13 +15,19 @@ class MyApp extends StatelessWidget {
 
 class RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
+  final _saved = Set<WordPair>();
   final _biggerFont = TextStyle(fontSize: 18.0);
 
   Widget _buildRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair);
     return ListTile(
       title: Text(
         pair.asPascalCase,
         style: _biggerFont,
+      ),
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
       ),
     );
   }
