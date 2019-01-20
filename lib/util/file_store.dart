86ea9cafb4f069c38bc9a7f7ecacd46f5c8a9597
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2019-01-20 10:08:20 -0800

    added a read file contents to pull the saved suggestions

--- lib/util/file_store.dart
+++ lib/util/file_store.dart
@@ -16,4 +16,20 @@ class FileStore {
     // Write the file
     return file.writeAsString(jsonEncode(saved.toList()));
   }
+
+  static Future<Set<String>> readCounter() async {
+    try {
+      final path = await _localPath;
+      final file = File('$path/saved_suggestions.json');
+
+      // Read the file
+      String contents = await file.readAsString();
+      Set<String> savedSuggestions = jsonDecode(contents);
+
+      return savedSuggestions;
+    } catch (e) {
+      // If we encounter an error, return 0
+      return new Set<String>();
+    }
+  }
 }
