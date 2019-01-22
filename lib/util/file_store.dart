cfd3c73158729c22a18687552d967f914522874f
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2019-01-22 10:52:40 -0800

    return a new set from the parsed list rather than assigning

--- lib/util/file_store.dart
+++ lib/util/file_store.dart
@@ -13,7 +13,6 @@ class FileStore {
     final path = await _localPath;
     final file = File('$path/saved_suggestions.json');
 
-    // Write the file
     return file.writeAsString(jsonEncode(saved.toList()));
   }
 
@@ -22,13 +21,10 @@ class FileStore {
       final path = await _localPath;
       final file = File('$path/saved_suggestions.json');
 
-      // Read the file
       String contents = await file.readAsString();
-      Set<String> savedSuggestions = jsonDecode(contents);
-
-      return savedSuggestions;
+      return Set<String>.from(jsonDecode(contents));
     } catch (e) {
-      // If we encounter an error, return 0
+      print(e);
       return new Set<String>();
     }
   }
